127ef689b132c2548959e270788030185b73eae5
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2018-12-19 07:49:58 -0300

    Slideshow

---
+++ slideshow/main.js
@@ -0,0 +1,21 @@
+let i = 0;
+const images = [];
+const time = 1000;
+
+images[0] = 'img1.jpg';
+images[1] = 'img2.jpg';
+images[2] = 'img3.jpg';
+images[3] = 'img4.jpg';
+
+function changeImg(){
+  document.slide.src = images[i];
+  if(i < images.length - 1){
+    i++;
+  }else{
+    i = 0;
+  }
+  
+  setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
