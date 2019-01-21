4fd2864b2da758e26ede5b4cc42a2c356dd49549
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2019-01-20 22:02:18 -0300

    Atualizando

--- image-gallery/main.js
+++ image-gallery/main.js
@@ -1,1 +1,23 @@
-const current = document.querySelector('#current');
+// const current = document.querySelector('#current');
+// const imgs = document.querySelectorAll('.imgs img');
+
+// destructuring assignment
+const [current, imgs] = [
+  document.querySelector('#current'),
+  document.querySelectorAll('.imgs img')
+];
+const opacity = 0.6;
+
+imgs[0].style.opacity = opacity;
+
+imgs.forEach(img => img.addEventListener('click', imgClick));
+
+function imgClick(e) {
+  imgs.forEach(img => (img.style.opacity = 1));
+  current.src = e.target.src;
+  current.classList.add('fade-in');
+  setTimeout(() => {
+    current.classList.remove('fade-in')
+  }, 500);
+  e.target.style.opacity = 0.4;
+}
