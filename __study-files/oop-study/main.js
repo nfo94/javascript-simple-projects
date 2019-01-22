9fbc75e6e134c4493538e49adc625e39190ad247
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2019-01-22 08:22:11 -0300

    Atualizando

--- __study-files/oop-study/main.js
+++ __study-files/oop-study/main.js
@@ -10,17 +10,39 @@ class User{
   constructor(name, email){
     this.name = name;
     this.email = email;
+    this.score = 0;
   }
   login(){
     console.log(this.email, 'is logged in');
+    return this;
   }
   logout(){
     console.log(this.email, 'is logged out');
+    return this;
+  }
+  updateScore(){
+    this.score++;
+    console.log(this.email, 'score is now', this.score);
+    return this; //method chaining
   }
 }
 
+class Admin extends User{ // inheritance
+  deleteUser(user){
+    users = users.filter(u =>{
+      return u.email != user.email;
+    })
+  }
+}
+
+let userTwo = new User('Marcelo', 'mfo@gmail.com');
 let userOne = new User('Natália', 'nfo@gmail.com');
-userOne.login();
+let adminOne = new Admin('Magaly', 'mmfo@gmail.com');
+let users = [userOne, userTwo, adminOne];
+adminOne.deleteUser(userTwo);
+console.log(users);
+
+// userOne.login().logout(); //method chaining
 
 /**
  * The "new" keyword creates a new empty object {}
