497e59aafa7ccd39944834a999f5bb0dbefa0f07
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2019-01-22 05:54:46 -0300

    Atualizando

--- __study-files/oop-study/main.js
+++ __study-files/oop-study/main.js
@@ -1,5 +1,19 @@
 /**
  * - Objects have properties and methods with key-value pairs 
  * - Arrays are objects 
- * - 
+ * - Primitive types are not objects (boolean, string, number, symbol, null, undefined)
+ * - But primitives can behave like objects because javascript can wrap them in an object
  */
+
+let userOne = {
+  email: 'nfo@ninja',
+  name: 'Nat',
+  login(){
+    console.log(this.email, 'has logged in');
+  },
+  logout(){
+    console.log(this.email, 'has logged out');
+  }
+};
+
+console.log(userOne.name);
