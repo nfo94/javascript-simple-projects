d6e3d4ad8e0991dfd544729c527521dd2a097efe
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2019-01-22 09:46:08 -0300

    Atualizando

--- __study-files/oop-study/main2.js
+++ __study-files/oop-study/main2.js
@@ -12,3 +12,17 @@ User.prototype.login = function(){
 
 let userOne = new User('nfo@gmail.com', 'Natália');
 console.log(userOne);
+let adminOne = new Admin('mfo@gmail.com', 'Marcelo');
+console.log(adminOne);
+
+function Admin(...args){
+  User.apply(this, args)
+  /*
+  *O método apply() chama uma função com um dado valor 
+  *this e arguments providos como uma array (ou um 
+  *objeto parecido com um array).
+  */
+ this.role = 'admin'; // a property tha only admin has
+}
+
+Admin.prototype = Object.create(User.prototype);
