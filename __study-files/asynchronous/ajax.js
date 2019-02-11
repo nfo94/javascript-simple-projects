1b36ca250abd0ed72fa5386fa167a151f50a2215
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2019-02-10 21:08:06 -0300

    Atualizando

--- __study-files/asynchronous/ajax.js
+++ __study-files/asynchronous/ajax.js
@@ -1,2 +1,17 @@
 // Ajax request
 
+window.onload = function(){
+  let http = new XMLHttpRequest();
+
+  http.onreadystatechange = function(){
+    if(http.readyState == 4 && http.status == 200){
+      console.log(JSON.parse(http.response));
+    }
+  };
+
+  http.open('GET', 'data/tweet.json', true);
+  http.send();
+  // after the 
+  console.log('test');
+  
+}
