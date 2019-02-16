973bf2804d8fff0eb2bc54cd9c41b03104c8b84b
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2019-02-16 17:39:55 -0300

    Atualizando

--- flappy-bird/js/flappy.js
+++ flappy-bird/js/flappy.js
@@ -1,15 +1,16 @@
 // to create a new element on html 
-function newElement(tagName, className) {
+function newElement(tagName, className) { //novoElemento
   // create element of the passed tag 
   const elem = document.createElement(tagName);
   // adding class to the new tag 
   elem.className = className;
-  // returnig the element 
+  // returning the element 
   return elem;
 }
 
 // creating new barrier
-function Barrier(reverse = false) { // creating one barrier box
+function Barrier(reverse = false) { //Barreira
+  // creating one barrier box
   this.element = newElement('div', 'barrier'); //new barrier element
 
   // creating the border and the body of the barrier
@@ -26,10 +27,9 @@ function Barrier(reverse = false) { // creating one barrier box
   this.setHeight = height => body.style.height = `${height}px`;
 }
 
-// create new obstacle and the opening space;
-// "gameHeight" is the screen, "opening" is the space for the bird 
-// to pass through and "x" is for the x axis - moving the animation
-function Obstacle(gameHeight, opening, x) {
+// create new obstacle: "opening" is the space for the bird to pass through and 
+// "x" is for the x axis - moving the animation
+function Obstacle(gameHeight, opening, x) { //ParDeBarreiras
   this.element = newElement('div', 'obstacle'); // the whole obstacle!
   this.topBarrier = new Barrier(true); // top barrier
   this.bottomBarrier = new Barrier(false); // bottom barrier
@@ -53,7 +53,7 @@ function Obstacle(gameHeight, opening, x) {
   // considering the x passed in the arguments, setting new x
   this.setX = x => this.element.style.left = `${x}px`;
   // "The clientWidth property returns the viewable width of an element 
-  // in pixels, including padding, but not the border, scrollbar or margin".
+  // in pixels, including padding, but not the border, scrollbar  or margin".
   this.getWidth = () => this.element.clientWidth;
   // calling the lottery opening function to choose randomly
   this.lotteryOpening();
@@ -61,7 +61,7 @@ function Obstacle(gameHeight, opening, x) {
   this.setX(x);
 }
 
-function multipleObstacles(gameHeight, gameWidth, opening, spaceBetweenObstacles, notifyGameCenter) {
+function multipleObstacles(gameHeight, gameWidth, opening, spaceBetweenObstacles, notifyGameMiddle) { //Barreiras
   this.obstacles = [
     new Obstacle(gameHeight, opening, gameWidth),
     new Obstacle(gameHeight, opening, gameWidth + spaceBetweenObstacles),
@@ -71,7 +71,7 @@ function multipleObstacles(gameHeight, gameWidth, opening, spaceBetweenObstacles
   // pixels to move x, animating
   const displacement = 3;
   this.animate = () => {
-    this.obstacles.forEach(obstacle => { 
+    this.obstacles.forEach(obstacle => {
       // set a new x based on the current x (get) and the displacement
       obstacle.setX(obstacle.getX() - displacement);
       // when the element gets out of the game screen
@@ -81,46 +81,106 @@ function multipleObstacles(gameHeight, gameWidth, opening, spaceBetweenObstacles
         // changing randomly the new opening
         obstacle.lotteryOpening();
       }
-
+      // screen's center
       const middle = gameWidth / 2;
-      const passedMiddle = obstacle.getX() + displacement >= middle && obstacle.getX() < middle
-      if (passedMiddle) notifyGameCenter();
+      const passedMiddle = obstacle.getX() + displacement >= middle && obstacle.getX() < middle;
+      if (passedMiddle) notifyGameMiddle();
     })
   }
 }
 
-function Bird(gameHeight) {
+// creating the bird and it's engine
+function Bird(gameHeight) { // Passaro
   let flying = false;
+  // new bird element
   this.element = newElement('img', 'bird');
+  // bird source
   this.element.src = './imgs/bird.png';
-
+  // get the Y axis where the bird is and parsing it
   this.getY = () => parseInt(this.element.style.bottom.split('px')[0]);
+  // pressing buttons
   this.setY = y => this.element.style.bottom = `${y}px`;
   window.onkeydown = e => flying = true;
   window.onkeyup = e => flying = false;
-
   this.animate = () => {
+    // flying? up 8 pixels. Not? -5 pixels
     const newY = this.getY() + (flying ? 8 : -5);
+    // the max. height it's the game screen 
     const maxHeight = gameHeight - this.element.clientHeight;
-
+    // stop bird from escaping the screen (bottom)
     if (newY <= 0) {
       this.setY(0);
-    } else if (newY >= maxHeight) {
+    } else if (newY >= maxHeight) { // stop the bird from escaping the screen (top)
       this.setY(maxHeight);
     } else {
-      this.setY(newY);
+      this.setY(newY); // in any other circumstances, just fly
     }
   }
 
   this.setY(gameHeight / 2)
 }
 
-const barriers = new multipleObstacles(700, 1200, 450, 400);
-const bird = new Bird(700);
-const gameArea = document.querySelector('.screen');
-gameArea.appendChild(bird.element);
-barriers.obstacles.forEach(obstacle => gameArea.appendChild(obstacle.element));
-setInterval(() => {
-  barriers.animate();
-  bird.animate();
-}, 20);
+// defining game progress
+function Progress() { // Progresso
+  // create new progress element
+  this.element = newElement('span', 'progress');
+  // method to put the updated score in the html
+  this.updateScore = score => this.element.innerHTML = score;
+  // beginning of scoring
+  this.updateScore(0);
+}
+
+function areOverlaped(elementA, elementB) {
+  // The Element.getBoundingClientRect() method returns the size of an element and 
+  // its position relative to the viewport.
+  const a = elementA.getBoundingClientRect();
+  const b = elementB.getBoundingClientRect();
+
+  // check horizontal and vertical collision
+  const horizontal = a.left + a.width >= a.left && b.left + b.width >= a.left;
+  const vertical = a.top + a.height >= b.top && b.top + b.height >= a.top;
+  return horizontal && vertical;
+}
+
+function elementsCollided(bird, multipleObstacles) {
+  let collided = false;
+  multipleObstacles.obstacles.forEach(obstacle => {
+    if (!collided) {
+      const above = obstacle.above.element;
+      const bellow = obstacle.bellow.element;
+      collided = areOverlaped(bird.element, above) || areOverlaped(bird.element, bellow)
+    }
+  })
+  return collided;
+}
+
+// start game
+function FlappyBird() {
+  let score = 0;
+  // game area, height and width 
+  const gameArea = document.querySelector('.screen');
+  const gameHeight = gameArea.clientHeight;
+  const gameWidth = gameArea.clientWidth;
+  //creating the progress 
+  const progress = new Progress();
+  // creating multiple obstacles
+  const multipleObstacles = new multipleObstacles(gameHeight, gameWidth, 250, 400, () => progress.updateScore(++score));
+  // creating birds
+  const bird = new Bird(gameHeight);
+  // appending stuff on game screen
+  gameArea.appendChild(progress.element);
+  gameArea.appendChild(bird.element);
+  multipleObstacles.obstacles.forEach(obstacle => gameArea.appendChild(obstacle.element));
+  // starting animation
+  this.start = () => {
+    const timer = setInterval(() => {
+      multipleObstacles.animate();
+      bird.animate();
+      if (collided(bird, multipleObstacles)) {
+        clearInterval(timer);
+      }
+    }, 20);
+  }
+}
+
+new FlappyBird().start();
