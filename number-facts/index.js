73dadd16bf7e1a8eef17296ee29976759160898e
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2019-02-21 20:49:17 -0300

    Atualizando

---
+++ number-facts/index.js
@@ -0,0 +1,35 @@
+let fact = document.querySelector('#fact');
+let factText = document.querySelector('#factText');
+let numberInput = document.querySelector('#numberInput');
+numberInput.addEventListener('input', getFactFetch); // change here to getFactAjax to test
+
+//  WITH XMLHttpRequest()
+
+// function getFactAjax(){
+//   let number = numberInput.value;
+//   let xhr = new XMLHttpRequest();
+//   xhr.open('GET', `http://numbersapi.com/${number}`);
+//   xhr.onload = function(){
+//     if(this.status == 200 && number != ''){
+//       fact.style.display = 'block';
+//       factText.innerHTML = this.responseText;
+//     }
+//   }
+
+//   xhr.send();
+// }
+
+// WITH FETCH API
+
+function getFactFetch(){
+  let number = numberInput.value;
+  fetch(`http://numbersapi.com/${number}`)
+    .then(response => response.text())
+    .then(data => { 
+      if(number != ''){
+        fact.style.display = 'block';
+        factText.innerHTML = data;
+      }
+    })
+    .catch(err => console.log(err));
+}
