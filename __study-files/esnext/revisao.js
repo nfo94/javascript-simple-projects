677d18940c11aed7cd598c95721f50b9b2b3a9d7
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2019-02-21 21:27:06 -0300

    Atualizando

---
+++ __study-files/esnext/revisao.js
@@ -0,0 +1,38 @@
+// let e var
+// {
+//   var a = 1;
+//   let b = 2;
+// }
+
+// console.log(a);
+// console.log(b);
+
+// template string
+
+// let product = 'iPad';
+// console.log(`${product}`);
+
+// destructuring
+
+// const [x, y] = [4, 5];
+// console.log(x, y);
+
+// const { name, age } = { name: 'Nat', age: 24 };
+// console.log(name, age);
+
+// const sum = (a, b) => a + b;
+// console.log(sum(2, 3));
+
+// arrow - this is associated with the local it was written
+
+// default parameter 
+
+// rest - aggregate
+// function total(...numbers){
+//   let total = 0;
+//   numbers.forEach(num => total += num);
+//   return total;
+// }
+
+// console.log(total(2, 4, 2, 2));
+
