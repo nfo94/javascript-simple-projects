1fbf5ba60c82120d63bce83d854347d3f0e90dd4
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2019-02-24 00:45:25 -0300

    Atualizando

--- typewriter-effect/main.js
+++ typewriter-effect/main.js
@@ -8,4 +8,29 @@ const Typewriter = function (txtElement, words, wait = 3000) {
   this.isDeleting = false;
 }
 
+Typewriter.prototype.type = function(){
+  const current = this.wordIndex % this.words.length;
+  const fullTxt = this.words[current];
+
+  // The substring() method extracts the characters from a string, 
+  // between two specified indices, and returns the new sub string.
+  if(this.isDeleting){
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  }else{
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+  setTimeout(() => {
+    this.type();
+  }, 400);
+}
+
 document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+  const txtElement = document.querySelector('.txt-type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+  new Typewriter(txtElement, words, wait);
+}
